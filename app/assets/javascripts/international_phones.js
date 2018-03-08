3c332b3aacea1251877e4a143eda2561b4185603
jordi cucala <jordicucala@hotmail.com>
2018-03-08 10:31:50 +0100

    fixed get parent node

--- app/assets/javascripts/international_phones.js
+++ app/assets/javascripts/international_phones.js
@@ -8,8 +8,8 @@ $(document).on('shown.bs.modal', function () { international_phones_init() });
 function international_phones_init () {
     $('[data-intl-tel-input]').each(function () {
         var element = $(this);
-        var hidden = element.parent().find('.international_phone_number');
-        var hidden_phone_country_code =  element.parent().find('.phone_country_code');
+        var hidden = element.closest('.form_phone_row').find('.international_phone_number');
+        var hidden_phone_country_code =  element.closest('.form_phone_row').find('.phone_country_code');
         element.intlTelInput(element.data('intl-tel-input') || {});
 
         element.closest('form').submit(function () {
